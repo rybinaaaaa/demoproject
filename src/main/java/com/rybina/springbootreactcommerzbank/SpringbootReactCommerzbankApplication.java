509f80b58250c60848c5a6c9d451732d16584ab7
rybinali <rybinaaaaa.a@gmail.com>
2024-10-21 20:37:48 +0200

    init

---
+++ src/main/java/com/rybina/springbootreactcommerzbank/SpringbootReactCommerzbankApplication.java
@@ -0,0 +1,13 @@
+package com.rybina.springbootreactcommerzbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactCommerzbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactCommerzbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rybina/springbootreactcommerzbank/SpringbootReactCommerzbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rybina.springbootreactcommerzbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactCommerzbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
