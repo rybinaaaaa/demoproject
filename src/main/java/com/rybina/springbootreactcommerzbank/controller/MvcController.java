b65588302cfdbeebdb568f1aeada2c87b4a3239f
rybinali <rybinaaaaa.a@gmail.com>
2024-10-24 21:25:09 +0200

    add redirects from backend

---
+++ src/main/java/com/rybina/springbootreactcommerzbank/controller/MvcController.java
@@ -0,0 +1,13 @@
+package com.rybina.springbootreactcommerzbank.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MvcController {
+
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
+    }
+}
