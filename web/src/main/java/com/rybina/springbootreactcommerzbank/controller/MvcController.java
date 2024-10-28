eb84c5d3f48ca9d29a835d16f3c37c09d40dc118
rybinali <rybinaaaaa.a@gmail.com>
2024-10-28 16:09:43 +0100

    optimize mvc forwarding

--- web/src/main/java/com/rybina/springbootreactcommerzbank/controller/MvcController.java
+++ web/src/main/java/com/rybina/springbootreactcommerzbank/controller/MvcController.java
@@ -6,13 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MvcController {
 
-    @RequestMapping(value = "/{path:[^.]*}")
+    @RequestMapping(value = "/**/{path:[^.]*}")
     public String redirect() {
-        return "forward:/index.html";
-    }
-
-    @RequestMapping(value = "/**/{subpath:[^.]*}")
-    public String redirectSubPathes() {
         return "forward:/index.html";
     }
 }
